ce12b760325bb889b80c8ac546d778ef1b1382a7
Nandhini S <108602908+Nandhini-154@users.noreply.github.com>
2026-05-18 14:35:18 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,24 +4,35 @@ import AdminUpload from "./pages/AdminUpload";
 import Catalogue from "./pages/Catalogue";
 import ProductView from "./pages/ProductView";
 import AdminList from "./pages/AdminList";
+
 function App() {
 
     return (
 
-        <Routes>
+        <>
 
-            <Route path="/" element={<Catalogue />} />
+            <Routes>
 
-            <Route path="/admin" element={<AdminUpload />} />
+                <Route path="/" element={<Catalogue />} />
 
-            <Route path="/product/:id" element={<ProductView />} />
-            <Route path="/manage" element={<AdminList />} />
-        </Routes>
-  <div>
-       <p>
-    © 2026 Sri Kavin's Tailors. Some images used on this website are sourced from external resources. All respective copyrights belong to their original owners.
-</p>
-        </div>
+                <Route path="/admin" element={<AdminUpload />} />
+
+                <Route path="/product/:id" element={<ProductView />} />
+
+                <Route path="/manage" element={<AdminList />} />
+
+            </Routes>
+
+            <div className="footer">
+
+                <p>
+                    © 2026 Sri Kavin's Tailors. Some images used on this website are sourced from external resources. All respective copyrights belong to their original owners.
+                </p>
+
+            </div>
+
+        </>
+
     );
 }
 
